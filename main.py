bbfc5e4a3bb14e9bcb53caad895e66f6b1a16147
Stushbush <98846688+Stushbush@users.noreply.github.com>
2022-02-03 21:13:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+name="Kisha"
+print(f"I am called {name}.")
+sex="female"
+print (f"I am a {sex} very tall with long limbs.")
+My_favorite_color="purple"
+#print(my_favorite_color)
+print(f"My favorite color is {My_favorite_color}.")
+My_favorite_holiday="Christmas"
+print(f"My favorite holiday is {My_favorite_holiday}.")
+My_cake_preference="red velvet"
+print("My cake preference is" ,My_cake_preference,".")
+Country="Antigua"
+print(f"I was born and raised in {Country}.")
+sport="Netball"
+print (f"I play {sport} during my spare time.")
+kid="Nyheim"
+print(f"God has blessed me with {kid} whom I love and adore.")
+dance="day party"
+print(f"I enjoy a good cooler {dance}.")
+food="lasagna"
+print (f"I love to eat so I chose {food} as my favorite dish.")
+hair="dreads"
+print (f"With the rising cost for beauty I am extremely happy that I chose {hair} to offset my beauty.")
+age=40.00
+print (f"Although I may not look it but I am {age}.")
+cake=False
+if cake:
+  print ("Kisha loves cake")
+else:
+  print ("Kisha hates cake")
